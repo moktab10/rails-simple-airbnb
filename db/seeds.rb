90d08130461fa8c5bacbb1841f67716d4d777362
Mokhtar BOUSSEFSAF <moktab10@gmail.com>
2020-05-14 14:57:26 +0200

    migrate + seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+puts "Start seed"
+puts "Flat Toulon was created"
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Toulon',
+  address: '10 Clifton Gardens Toulon W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+puts "Flat Marseille was created"
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Marseille',
+  address: '11 Clifton Gardens Marseille W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms',
+  price_per_night: 90,
+  number_of_guests: 2
+)
+puts "Flat Nice was created"
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Nice',
+  address: '12 Clifton Gardens Nice W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat',
+  price_per_night: 60,
+  number_of_guests: 1
+)
+puts "Flat Paris was created"
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Paris',
+  address: '13 Clifton Gardens Paris W9 1DT',
+  description: 'I like this flat',
+  price_per_night: 80,
+  number_of_guests: 2
+)
+puts "Flat Nimes was created"
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Nimes',
+  address: '14 Clifton Gardens Nimes W9 1DT',
+  description: 'A lovely summer flat',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+puts "End seed"
