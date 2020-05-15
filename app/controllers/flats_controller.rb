d9bf631a43f7c8123ab7d260c505daa20c9d5e92
Mokhtar BOUSSEFSAF <moktab10@gmail.com>
2020-05-15 13:01:37 +0200

    airbnb non search

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,10 +1,10 @@
 class FlatsController < ApplicationController
   def index
-    @flats = Flat.all
+      @flats = Flat.all
   end
 
   def show
-    @flat = Flat.new(params[:id])
+    @flat = Flat.find(params[:id])
   end
 
   def new
@@ -12,14 +12,33 @@ class FlatsController < ApplicationController
   end
 
   def edit
+    @flat = Flat.find(params[:id])
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
   end
 
   def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
   end
 
   def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture_url)
   end
 end
